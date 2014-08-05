2322d1e57399ae448a4dd4709851438aa4343f94
Randy Sesser <rsesser@local.com>
2014-08-04 22:21:00 -0700

    Converting to Guzzle 4+
    - Added before, complete and error event support for the HTTP client
    - Added option to pass `verify` option

--- lib/Jcroll/FoursquareApiClient/Client/FoursquareClient.php
+++ lib/Jcroll/FoursquareApiClient/Client/FoursquareClient.php
@@ -6,21 +6,30 @@ use GuzzleHttp\Collection;
 use GuzzleHttp\Command\Guzzle\GuzzleClient;
 use GuzzleHttp\Command\Guzzle\Description;
 use GuzzleHttp\Subscriber\Oauth\Oauth1;
+use GuzzleHttp\Event\BeforeEvent;
 use \InvalidArgumentException;
 
 class FoursquareClient extends GuzzleClient
 {
     static $serviceDescription = NULL;
+    private $httpClient = NULL;
     public static function factory($config = array())
     {
         if (static::$serviceDescription == NULL) {
             static::$serviceDescription = json_decode(file_get_contents(dirname(__DIR__).'/Resources/config/client.json'), true);
         }
 
-        $default = array('base_url' => 'https://api.foursquare.com/v2/');
+//        static::$serviceDescription['base_url'] = 'https://api.foursquare.com/v2/';
+
+        $default = array(
+            'verify' => true,
+            'event.before' => array(),
+            'event.after' => array()
+        );
+
         $required = array(
             'client_id',
-            'client_secret',
+            'client_secret'
         );
 
         foreach ($required as $value) {
@@ -29,24 +38,38 @@ class FoursquareClient extends GuzzleClient
             }
         }
 
+
         $description = new Description(static::$serviceDescription);
 
         $config = Collection::fromConfig($config, $default, $required);
-        $client = new Client(array(
-            'base_url' => $config->get('base_url'),
-            'query' => array(
-                'client_id' => $config['client_id'],
-                'client_secret' => $config['client_secret'],
-                'v' => '20130707'
-            ),
-            'defaults' => array('auth' => 'oauth')
+
+        $httpClient = new Client(array(
+            'defaults' => array(
+                'query' => array(
+                    'client_id' => $config['client_id'],
+                    'client_secret' => $config['client_secret'],
+                    'v' => '20130707'
+                ),
+                'verify' => $config['verify']
+            )
         ));
-        $oauth = new Oauth1(array(
-            'consumer_key' => $config['client_id'],
-            'consumer_secret' => $config['client_secret']
-        ));
-        $client->getEmitter()->attach($oauth);
-        return new self($client, $description);
+
+        if (is_callable($config['event.before']))
+            $httpClient->getEmitter()->on('before', $config['event.before']);
+
+        if (is_callable($config['event.complete']))
+            $httpClient->getEmitter()->on('complete', $config['event.complete']);
+
+        if (is_callable($config['event.error']))
+            $httpClient->getEmitter()->on('error', $config['event.error']);
+
+//        $oauth = new Oauth1(array(
+//            'consumer_key' => $config['client_id'],
+//            'consumer_secret' => $config['client_secret']
+//        ));
+//         $httpClient->getEmitter()->attach($oauth);
+
+        return new self($httpClient, $description);
     }
 
     public function addToken($token)
