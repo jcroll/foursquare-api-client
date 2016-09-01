5c27b942299c370a6376be723df50063b507d10a
Jarrett Croll <jarrett.croll@gmail.com>
2016-09-01 14:53:06 +0000

    allow version configuration

--- src/Client/FoursquareClient.php
+++ src/Client/FoursquareClient.php
@@ -21,18 +21,26 @@ class FoursquareClient extends GuzzleClient
             }
         }
 
+        $version = isset($config['version']) ? (int) $config['version'] : 20160901;
+        $mode    = isset($config['mode']) ? $config['mode'] : 'foursquare';
+
+        static::validateVersion($version);
+        static::validateMode($mode);
+
         $client = new Client([
             'base_url' => 'https://api.foursquare.com/v2/',
             'defaults' => [
                 'query' => [
                     'client_id'     => $config['client_id'],
                     'client_secret' => $config['client_secret'],
-                    'v'             => '20130707'
+                    'v'             => $version,
+                    'm'             => $mode,
                 ],
             ]
         ]);
 
-        $contents    = file_get_contents(sprintf('%s/../Resources/config/client.json', __DIR__));
+        $directory   = $version >= 20160901 ? 20160901 : 20130707;
+        $contents    = file_get_contents(sprintf('%s/../Resources/config/%s/client.json', __DIR__, $directory));
         $description = new Description(json_decode($contents, true));
 
         return new static($client, $description);
@@ -45,10 +53,58 @@ class FoursquareClient extends GuzzleClient
      */
     public function addToken($token)
     {
-        $query = $this->getHttpClient()->getDefaultOption('query');
-        $query['oauth_token'] = $token;
-        $this->getHttpClient()->setDefaultOption('query', $query);
+        $this->getHttpClient()->setDefaultOption('query/oauth_token', $token);
 
         return $this;
+    }
+
+    /**
+     * @param string $mode
+     *
+     * @return $this
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function setMode($mode)
+    {
+        static::validateMode($mode);
+
+        $this->getHttpClient()->setDefaultOption('query/m', $mode);
+
+        return $this;
+    }
+
+    /**
+     * @param int $version
+     *
+     * @throws \InvalidArgumentException
+     */
+    private static function validateVersion($version)
+    {
+        if (8 === strlen($version)) {
+            $month = (int) substr($version, 4, 2);
+            $day   = (int) substr($version, 6, 2);
+            $year  = (int) substr($version, 0, 4);
+
+            if (checkdate($month, $day, $year)) {
+                return;
+            }
+        }
+
+        throw new \InvalidArgumentException(sprintf('"%d" is an invalid version.', $version));
+    }
+
+    /**
+     * @param string $mode
+     *
+     * @throws \InvalidArgumentException
+     */
+    private static function validateMode($mode)
+    {
+        $modes = ['foursquare', 'swarm'];
+
+        if (!in_array($mode, $modes)) {
+            throw new \InvalidArgumentException('Acceptable values for "mode" are "foursquare" or "swarm".');
+        }
     }
 }
